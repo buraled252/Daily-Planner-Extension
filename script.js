3bdfeb2855d260d1c62c81d5ff66d000ac6b5742
burale252 <burale252>
2023-06-06 11:33:40 -0400

    fetch tasks from local storage"

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ const addTaskButton = document.getElementById("addTaskButton")
 const taskList = document.getElementById("taskList")
 
 let taskArray = getTasksFromLocalStorage()
+
+function getTasksFromLocalStorage(){
+    return JSON.parse(localStorage.getItem("tasks")) || [];
+}
